ea2870e1f26407f8bc2d627f7211919606d5c15a
Danil Shapilov <danil_shapilov@amazingcat.net>
2025-01-12 22:38:56 +0500

    rotation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE, SCREEN_HEIGHT,SCREEN_WIDTH
+from player import Player
 
 def main():
     pygame.init()
@@ -7,12 +8,19 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0), (0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
+        player.draw(screen)
+        player.update(dt)
+        
         pygame.display.flip()
+        dt = clock.tick(60)  / 1000
 
 if __name__ == "__main__":
     main()
